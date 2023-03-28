8ecdd1d3d4d6ccb21fa99898185631b5db6cc12b
maimuna omar <maymunaabdi07@gmail.com>
2023-03-28 14:35:41 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Write your code here 
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 12;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const mod = num3 % num4;
+
+const numbers = [1, 5, 20, 10, 7];
+
+const max = Math.max(...numbers);
